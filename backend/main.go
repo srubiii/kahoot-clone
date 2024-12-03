bb25602fc5a1e3b100338f8bd37f4c9b7aa5280d
Sarah <sarahkrubi@gmail.com>
2024-12-02 23:49:26 -0500

    backend update

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", index)
+	app.Get("/api/quizzes", getQuizzes)
 
-    log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3000"))
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	list := []map[string]any{
+		{
+			"test": 123,
+		},
+	}
+	return c.JSON(list)
 }
