0a4346895e720e646a47411fb1bb15be7c070da3
Sarah <sarahkrubi@gmail.com>
2024-12-03 22:07:07 -0500

    more changes

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"time"
 
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -16,10 +18,37 @@ var quizCollection *mongo.Collection
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
 	setupDb()
 
 	app.Get("/", index)
 	app.Get("/api/quizzes", getQuizzes)
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+
+	}))
 
 	log.Fatal(app.Listen(":3000"))
 }
@@ -27,10 +56,14 @@ func main() {
 func setupDb() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx,
-		options.Client().ApplyURI("mongodb://localhost:27017"))
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(clientOptions)
 	if err != nil {
 		panic(err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err) // Log and exit if ping fails
 	}
 	quizCollection = client.Database("quiz").Collection("quizzes")
 }
@@ -45,14 +78,11 @@ func getQuizzes(c *fiber.Ctx) error {
 		panic(err)
 	}
 
-	var quizzes []map[string]any
-	cursor.All(context.Background(), &quizzes)
-
-	
-	list := []map[string]any{
-		{
-			"test": 123,
-		},
+	quizzes := []map[string]any{}
+	err = cursor.All(context.Background(), &quizzes)
+	if err != nil {
+		panic(err)
 	}
-	return c.JSON(list)
+
+	return c.JSON(quizzes)
 }
